bc14e809d20aa0f9f1dc3cd57b08fa70701d3fae
jmu303 <muj@bc.edu>
2014-10-25 17:14:06 -0400

    Stylistc corrections, change n_build function def
    N_build can be a toplevel declaration, instead just taking in "newtext"
    as an argument.
    
    Minor stylistic corrections (js-beautify), semicolons, etc.

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
  * @author Tyler Chen, Jesse Mu
  */
 
-var observerConfig = {
-    childList: true,
-    subtree: true
-};
+var checkExist = setInterval(function() {
+   if (document.getElementById("webMessengerRecentMessages") !== null) {
+      monospaceInit();
+      clearInterval(checkExist);
+   }
+}, 50);
 
-function monospaceListen() {
-    console.log("calling monospace...");
-    var monospaceObserver = new MutationObserver(function(ms) {
-        ms.forEach(function(m) {
-            console.log(m.type);
-        });
-    });
-    monospaceObserver.observe(document.body, observerConfig);
+function monospaceInit() {
+    window.msgWindow = document.getElementById("webMessengerRecentMessages");
+    update();
+    // TODO: Update to just draw new elements.
+    msgWindow.addEventListener('DOMNodeInserted', update, false);
 }
 
 var start = "`~";
@@ -24,61 +23,40 @@ var l = 2;
 
 var text = [];
 
-function detect() {
-   var msgWindow = document.getElementById("webMessengerRecentMessages");
-   //^ something still sketchy about this.
+function update() {
+    // TODO: fix discrepancies here
+    var msgWindow = document.getElementById("webMessengerRecentMessages");
+    var chat = msgWindow.getElementsByClassName("_38");
 
-    //characters for codeblock
-    
-
-    var chat = msgWindow.getElementsByClassName("_38"); 
-
-    //FB large chat specific
+    // FB large chat specific
     for (i = chat.length-1; i >= 0; i--) {
-        draw( chat[i].getElementsByTagName("p")[0] )
+        draw(chat[i].getElementsByTagName("p")[0]);
     }
 }
 
-//run draw only on new element
-function update(e) {
-    console.log("update");
-    console.log(e);
-    if(true){
-        detect();
+function nBuild(newtext) {
+    n = "";
+    for (i = 1; i < newtext.split('\n').length + 1; i++) {
+        n += "<br/>" + i;
     }
+    return n.substring(5, n.length);
 }
 
-function copy(e){
-    console.log("!!!");    
-}
-
-function draw( em ) {
-    em.addEventListener("click", copy, false);
-    
+function draw(em) {
     words = em.innerText;
     var stop_index = words.indexOf(stop);
     var start_index = words.indexOf(start);
     // console.log( words.indexOf( "\n" ), start_index, stop_index );
 
     if (stop_index > start_index && start_index > -1) {
-        
+
         var newtext = words.substr(start_index + l, stop_index - start_index - l);
-        var newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g,'');
-        
-        var numbers  = n_build();
-        function n_build(){
-            n = ""
-            for(i=1; i<newtext.split('\n').length+1; i++){
-                n += "<br/>" + i;
-            }
-            return n.substring(5, n.length);
-         }
-        
+        newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g, '');
+
+        var numbers = nBuild(newtext);
+
         em.className += " code";
-        em.innerHTML = "<div class='numbers'>"+ numbers +"</div><pre class='text'>" + newtext + "</pre>";
-        hljs.highlightBlock(em.getElementsByClassName( 'text' )[0]);
+        em.innerHTML = "<div class='numbers'>" + numbers + "</div><pre class='text'>" + newtext + "</pre>";
+        hljs.highlightBlock(em.getElementsByClassName('text')[0]);
     }
-
 }
-
-monospaceListen();
--- background.js
+++ background.js
@@ -5,4 +5,8 @@
 
 var script = document.createElement("script");
 script.src = chrome.extension.getURL("app.js");
-document.head.appendChild(s);
+document.head.appendChild(script);
+
+var highlightjs = document.createElement("script");
+highlightjs.src = chrome.extension.getURL("highlight/highlight.pack.js");
+document.head.appendChild(highlightjs);
