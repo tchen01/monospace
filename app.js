bd3e26a7e042931d7dc338295fc952a9655f5543
jmu303 <muj@bc.edu>
2014-10-25 17:14:06 -0400

    Stylistc corrections, change n_build function def
    N_build can be a toplevel declaration, instead just taking in "newtext"
    as an argument.
    
    Minor stylistic corrections (js-beautify), semicolons, etc.
    
    TODO: an idea - "use strict"

--- app.js
+++ app.js
@@ -25,17 +25,17 @@ var l = 2;
 var text = [];
 
 function detect() {
-   var msgWindow = document.getElementById("webMessengerRecentMessages");
-   //^ something still sketchy about this.
+    var msgWindow = document.getElementById("webMessengerRecentMessages");
+    //^ something still sketchy about this.
 
     //characters for codeblock
-    
 
-    var chat = msgWindow.getElementsByClassName("_38"); 
+
+    var chat = msgWindow.getElementsByClassName("_38");
 
     //FB large chat specific
-    for (i = chat.length-1; i >= 0; i--) {
-        draw( chat[i].getElementsByTagName("p")[0] )
+    for (i = chat.length - 1; i >= 0; i--) {
+        draw(chat[i].getElementsByTagName("p")[0]);
     }
 }
 
@@ -43,40 +43,41 @@ function detect() {
 function update(e) {
     console.log("update");
     console.log(e);
-    if(true){
+    if (true) {
         detect();
     }
 }
 
-function copy(e){
-    console.log("!!!");    
+function copy(e) {
+    console.log("!!!");
 }
 
-function draw( em ) {
+function n_build(newtext) {
+    n = "";
+    for (i = 1; i < newtext.split('\n').length + 1; i++) {
+        n += "<br/>" + i;
+    }
+    return n.substring(5, n.length);
+}
+
+function draw(em) {
     em.addEventListener("click", copy, false);
-    
+
     words = em.innerText;
     var stop_index = words.indexOf(stop);
     var start_index = words.indexOf(start);
     // console.log( words.indexOf( "\n" ), start_index, stop_index );
 
     if (stop_index > start_index && start_index > -1) {
-        
+
         var newtext = words.substr(start_index + l, stop_index - start_index - l);
-        var newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g,'');
-        
-        var numbers  = n_build();
-        function n_build(){
-            n = ""
-            for(i=1; i<newtext.split('\n').length+1; i++){
-                n += "<br/>" + i;
-            }
-            return n.substring(5, n.length);
-         }
-        
+        newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g, '');
+
+        var numbers = n_build(newtext);
+
         em.className += " code";
-        em.innerHTML = "<div class='numbers'>"+ numbers +"</div><pre class='text'>" + newtext + "</pre>";
-        hljs.highlightBlock(em.getElementsByClassName( 'text' )[0]);
+        em.innerHTML = "<div class='numbers'>" + numbers + "</div><pre class='text'>" + newtext + "</pre>";
+        hljs.highlightBlock(em.getElementsByClassName('text')[0]);
     }
 
 }
