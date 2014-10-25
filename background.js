0701422da23dc7527ef056444aeb3895b3ad6e22
Tyler Chen <chentylerl@gmail.com>
2014-10-25 10:11:32 -0400

    excess line breaks removed, node insertion function created

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ monospaceInit();
 function monospaceInit() {
     window.msgWindow = document.getElementById("webMessengerRecentMessages");
     detect();
-    msgWindow.addEventListener('DOMNodeInserted', detect, false);
+    msgWindow.addEventListener('DOMNodeInserted', update, false);
     //^ update to just draw new elements.
 }
 
@@ -23,11 +23,17 @@ function detect() {
     var chat = msgWindow.getElementsByClassName("_38"); 
 
     //FB large chat specific
-    for (i = 0; i < chat.length; i++) {
+    for (i = chat.length-1; i >= 0; i--) {
         draw( chat[i].getElementsByTagName("p")[0] )
     }
 }
 
+//run draw only on new element
+function update(e) {
+    console.log("update");
+    console.log(e);
+    detect();
+}
 
 function draw( em ) {
     words = em.innerText;
@@ -36,17 +42,18 @@ function draw( em ) {
     console.log( words.indexOf( "\n" ), start_index, stop_index );
 
     if (stop_index > start_index && start_index > -1) {
-            console.log( words.substring(start_index + l, stop_index) );
+            // console.log( words.substring(start_index + l, stop_index) );
         
         var newtext = words.substr(start_index + l, stop_index - start_index - l);
+        var newtext = newtext.replace(/^\s+|\s+$/g,"");
+        
         var numbers  = n_build();
         function n_build(){
             n = ""
             for(i=1; i<newtext.split('\n').length+1; i++){
-                console.log( i );
                 n += "<br/>" + i;
-                console.log( n );
             }
+            console.log(n)
             return n.substring(5, n.length);
          }
         
