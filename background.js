8f0d0a083ad60d9f1ce3bdf7618a459e1730b402
Tyler Chen <chentylerl@gmail.com>
2014-10-25 11:42:28 -0400

    FB fully functional

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
-monospaceInit();
+var checkExist = setInterval(function() {
+   if (document.getElementById("webMessengerRecentMessages")) {
+      console.log("Exists!");
+      monospaceInit();
+      clearInterval(checkExist);
+   }
+}, 50);
+
+
 
 function monospaceInit() {
     window.msgWindow = document.getElementById("webMessengerRecentMessages");
@@ -7,12 +15,12 @@ function monospaceInit() {
     //^ update to just draw new elements.
 }
 
-var start = "`~ ";
-var stop = " ~`";
-var l = 3;
+var start = "`~";
+var stop = "~`";
+var l = 2;
 
 var text = [];
-    
+
 function detect() {
    var msgWindow = document.getElementById("webMessengerRecentMessages");
    //^ something still sketchy about this.
@@ -32,20 +40,27 @@ function detect() {
 function update(e) {
     console.log("update");
     console.log(e);
-    detect();
+    if(true){
+        detect();
+    }
+}
+
+function copy(e){
+    console.log("!!!");    
 }
 
 function draw( em ) {
+    em.addEventListener("click", copy, false);
+    
     words = em.innerText;
     var stop_index = words.indexOf(stop);
     var start_index = words.indexOf(start);
-    console.log( words.indexOf( "\n" ), start_index, stop_index );
+    // console.log( words.indexOf( "\n" ), start_index, stop_index );
 
     if (stop_index > start_index && start_index > -1) {
-            // console.log( words.substring(start_index + l, stop_index) );
         
         var newtext = words.substr(start_index + l, stop_index - start_index - l);
-        var newtext = newtext.replace(/^\s+|\s+$/g,"");
+        var newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g,'');
         
         var numbers  = n_build();
         function n_build(){
@@ -53,12 +68,12 @@ function draw( em ) {
             for(i=1; i<newtext.split('\n').length+1; i++){
                 n += "<br/>" + i;
             }
-            console.log(n)
             return n.substring(5, n.length);
          }
         
         em.className += " code";
-        em.innerHTML = "<div class='numbers'>"+ numbers +"</div><div class='text'>" + newtext + "</div>";
+        em.innerHTML = "<div class='numbers'>"+ numbers +"</div><pre class='text'>" + newtext + "</pre>";
+        hljs.highlightBlock(em.getElementsByClassName( 'text' )[0]);
     }
 
 }
