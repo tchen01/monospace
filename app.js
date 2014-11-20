a1ef9f4bd532e1c0154f5fc7bea57f910a2b09c6
Tyler Chen <chentylerl@gmail.com>
2014-11-20 08:08:19 -0500

    update draw function
    implemented most recent draw function from delimiters branch. This is
    not the most complete branch

--- app.js
+++ app.js
@@ -10,76 +10,107 @@ var observerConfig = {
     attributes: false
 };
 
-var cfx = []; //array containing all '_3hi clearfix' div 
+var cfx = []; //ideally array containing all '_3hi clearfix' div 
 //there is probably a way to avoid having to recreate this every mutation.
 
 function monospaceListen() {
     var monospaceObserver = new MutationObserver(function(ms) {
         ms.forEach(function(m) {
             if (m.addedNodes.length > 0) {
-                //console.log(m.addedNodes.className);
                 console.log(m.addedNodes);
                 for(l=0; l<m.addedNodes.length; l++){
-                    console.log(m.addedNodes[l].getElementsByClassName( "_3hi clearfix" ));
-                    var cfx = m.addedNodes[l].getElementsByClassName( "_3hi clearfix" );
+                    console.log(m.addedNodes[l].getElementsByClassName( "_38" )); 
+                    
+                    //there is probably a quicker way to get these elements.
+                    var cfx = m.addedNodes[l].getElementsByClassName( "_38" );
                     for(c=0; c<cfx.length; c++){
-                      draw(cfx[c].firstChild.firstChild.firstChild);
+                      draw(cfx[c].firstChild.firstChild);
+                      //%div._38 -> %span -> %p
                     }
                     //cfx = cfx.concat( m.addedNodes[l].getElementsByClassName( "_3hi clearfix" )[0] ); //Y I AM DUM?? 
                 }
                 console.log( "cfx: " + cfx );
-                // cfx.forEach(function( p ) {
-                  // console.log( p );
-                  // // draw(p.getElementsByTagName( "p" )[0]);
-                // });
-                /*
-                if (m.addedNodes.className === "webMessengerMessageGroup clearfix") { 
-                //this returns an array with 18 items where 1-17 are li containing text. 
-                  console.log( "found: " + m.addedNodes );
-                  
-                    
-                    //draw(m.addedNodes[0].firstChild.firstChild.firstChild);
-                }
-                */
             }
         });
     });
-    // TODO change event listener location depending on major website
     monospaceObserver.observe(document.getElementById('webMessengerRecentMessages'), observerConfig);
 }
 
-var start = "`~";
-var stop = "~`";
-var l = 2;
-
-var text = [];
-
+//this should return nothing if only one line
 function nBuild(newtext) {
     n = "";
-    for (i = 1; i < newtext.split('\n').length + 1; i++) {
-        n += "<br/>" + i;
+    for (m = 1; m < newtext.split('\n').length + 1; m++) {
+        n += "<br/>" + m;
     }
     return n.substring(5, n.length);
 }
 
+var del = "```";
+var rev = "```"; //dynamically reverse del
+
+
+var text = [];
+
+
 function draw(em) {
-    words = em.innerText;
-    var stop_index = words.indexOf(stop);
-    var start_index = words.indexOf(start);
-    // console.log( words.indexOf( "\n" ), start_index, stop_index );
+    text = em.innerText;
+    console.log( text );
+    var stop_index = text.lastIndexOf(rev);
+    var start_index = text.indexOf(del);
 
     if (stop_index > start_index && start_index > -1) {
+        var regexExpression = "("+del+"|"+rev+")";
+        var regex = new RegExp(regexExpression, "g");
+        var texts = text.split( regex );
+        var codes = text.match( regex );
+        
+        var newtext = "";
+        var text_hold = "";
+        var code_hold = "";
+        var state = 0; //1: after code start delimiter but before stop
+        var words = text.split( regex );
+        console.log( words );
+        
+        //http://jsfiddle.net/m9eLk17a/1/
+        function write(){
+          for(i = 0; i < words.length; i++){
+              if( state == 0){
+                if( words[i] == del){
+                    newtext += "<p>" + text_hold + "</p>";
+                    state = 1;
+                    text_hold = "";
+                } else {
+                    text_hold += words[i];
+                }
+            } else { //state == 1
+                if( words[i] == rev ){
+                    num = nBuild( code_hold );
+                    if( num == 1){
+                      newtext += "<div class='code inline'>"; //linebreaks at intersections having some issues.
+                    } else {
+                      newtext += "<div class='code'><div class='block_container'><div class='numbers'>" + nBuild( code_hold ) +"</div>";
+                    }
+                    newtext += "<pre class='text'>" + code_hold + "</pre></div></div>";
+                    console.log( code_hold );
+                    state = 0;
+                    code_hold = "";
+                    } else {
+                        code_hold += words[i];
+                    }
+                }
+            }
+            newtext += "<p>" + text_hold + code_hold + "</p>"; //would prefern not to change <p> to inline. Either add class or change tag
+            console.log( newtext );
+        }
+        write();
 
-        var newtext = words.substr(start_index + l, stop_index - start_index - l);
-        newtext = newtext.replace(/^[\r\n]+|[\r\n]+$/g, '');
-
-        var numbers = nBuild(newtext);
-
-        em.className += " code";
-        em.innerHTML = "<div class='numbers'>" + numbers + "</div><pre class='text'>" + newtext + "</pre>";
-        hljs.highlightBlock(em.getElementsByClassName('text')[0]);
+       /* IMPORTANT */
+        //how do <p> get into %div.code???
+        //running refresh too many times and not ignoring drawn sections?
+        
+        em.outerHTML = newtext;
+        console.log( em );
     }
-
 }
 
 var checkForMsg = setInterval(function() {
