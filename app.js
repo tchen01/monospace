24700ad421da7dc5d125a7f038977d94f774da49
Tyler Chen <chentylerl@gmail.com>
2014-11-22 21:54:31 -0500

    small chat functioning
    code draws but always inline.
    write function needs to be updated to the format of the small chat

--- app.js
+++ app.js
@@ -19,24 +19,28 @@ var rev = del.split('').reverse().join('');
 //should only be used on https://www.facebook.com/messages
 function monospaceListen() {
     var monospaceObserver = new MutationObserver(function(ms) {
-        ms.forEach(function(m) {
-            if (m.addedNodes.length > 0) { //do we even need this part?
-                console.log(m.addedNodes);
-                for(l=0; l<m.addedNodes.length; l++){
-                   
-                    //there is probably a quicker way to get these elements.
-                    var cfx = m.addedNodes[l].getElementsByClassName( "_38" );
-                    
-                    for(c=0; c<cfx.length; c++){
-                      draw(cfx[c].firstChild);
-                      //%div._38 -> %span -> %p
-                    }
+        // console.log(ms);
+        for(var m = 0; m<ms.length; m++){
+            // console.log(ms[m].addedNodes);
+            if( ms[m].addedNodes.length > 0){
+              // console.log( typeof( ms[m].addedNodes[0].className ) );
+              if( typeof( ms[m].addedNodes[0].className ) === 'string' ){
+                if( ms[m].addedNodes[0].className.indexOf( '_5wd4' ) > -1 ){
+                  // console.log( ms[m].addedNodes[0] );
+                  var cfx = ms[m].addedNodes[0].getElementsByClassName( '_5wdf' );
+                  // console.log( cfx );
+                  for(c=0; c<cfx.length; c++){
+                    // console.log( cfx[c].firstChild.firstChild );
+                    draw(cfx[c].firstChild.firstChild);
+                  }
                 }
+              }
             }
-        });
+        }
     });
-    monospaceObserver.observe(document.getElementById('webMessengerRecentMessages'), observerConfig);
+    monospaceObserver.observe(document.getElementById('ChatTabsPagelet'), observerConfig);
 }
+
 
 //I believe that these functions should work in the small chat as is
 function nBuild(nt) {
@@ -54,11 +58,12 @@ function draw(em) {
     var start_index = html.indexOf(del);
 
     if (stop_index > start_index && start_index > -1) {
+      console.log("inside");
       em.innerHTML = write(html);
       var code = em.getElementsByTagName( "pre" );
       for(var i=0; i<code.length; i++){
         console.log( code[i] );
-        hljs.highlightBlock( code[i] );
+        //hljs.highlightBlock( code[i] );
       }
     }
 }
@@ -67,6 +72,9 @@ function draw(em) {
 //takes innterHTML of parent element of %p tag(s) and formats to code style blocks 
 //http://jsfiddle.net/m9eLk17a/1/  
 function write(t){
+  console.log( t) ;
+  
+  //this doesm't work properly for small chat since they are %span elements and have a lot of extra stuff inside them
   var text = t.replace(del+"\n", del).replace("\n" + rev, rev).replace(/\n/g, "<br/>").replace(/<\/p><p>/g, "<br/><br/>").replace(/<\/p>|<p>/g,""); 
   
   console.log( text );
@@ -111,7 +119,7 @@ function write(t){
     return newtext;
 }
 var checkForMsg = setInterval(function() {
-    var msgWindow = document.getElementById('webMessengerRecentMessages');
+    var msgWindow = document.getElementById('ChatTabsPagelet');
     if (msgWindow !== null) {
         console.log('found!');
         monospaceListen(msgWindow, observerConfig);
