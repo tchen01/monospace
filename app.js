a668ab8fbf6ee2672e5f7188f4ed25ba43894893
Tyler Chen <chentylerl@gmail.com>
2014-12-29 14:00:58 -0600

    combine smallchat and regular
    one js file for small and regular chat. perhaps it would make more sense
    to have seperate js files since the large file doesn't need to be run
    except on facebook.com/messages

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ var rev = del.split('').reverse().join('');
 //should only be used on https://www.facebook.com/messages
 function monospaceListen() {
     var monospaceObserver = new MutationObserver(function(ms) {
+        ms.forEach(function(m) {
+            if (m.addedNodes.length > 0) { //do we even need this part?
+                console.log(m.addedNodes);
+                for(l=0; l<m.addedNodes.length; l++){
+                   
+                    //there is probably a quicker way to get these elements.
+                    var cfx = m.addedNodes[l].getElementsByClassName( "_38" );
+                    
+                    for(c=0; c<cfx.length; c++){
+                      draw(cfx[c].firstChild);
+                      //%div._38 -> %span -> %p
+                    }
+                }
+            }
+        });
+    });
+    monospaceObserver.observe(document.getElementById('webMessengerRecentMessages'), observerConfig);
+}
+
+function SMmonospaceListen() {
+    var SMmonospaceObserver = new MutationObserver(function(ms) {
         // console.log(ms);
         for(var m = 0; m<ms.length; m++){
             // console.log(ms[m].addedNodes);
@@ -38,7 +59,7 @@ function monospaceListen() {
             }
         }
     });
-    monospaceObserver.observe(document.getElementById('ChatTabsPagelet'), observerConfig);
+    SMmonospaceObserver.observe(document.getElementById('ChatTabsPagelet'), observerConfig);
 }
 
 
@@ -58,13 +79,13 @@ function draw(em) {
     var start_index = html.indexOf(del);
 
     if (stop_index > start_index && start_index > -1) {
-      console.log("inside");
-      em.innerHTML = write(html);
-      var code = em.getElementsByTagName( "pre" );
-      for(var i=0; i<code.length; i++){
-        console.log( code[i] );
-        hljs.highlightBlock( code[i] );
-      }
+        console.log("inside");
+        em.innerHTML = write(html);
+        var code = em.getElementsByTagName( "pre" );
+        for(var i=0; i<code.length; i++){
+            console.log( code[i] );
+            hljs.highlightBlock( code[i] );
+        }
     }
 }
 
@@ -72,21 +93,21 @@ function draw(em) {
 //takes innterHTML of parent element of %p tag(s) and formats to code style blocks 
 //http://jsfiddle.net/m9eLk17a/1/  
 function write(t){
-  console.log( t) ;
-  
-  var text = t.replace(del+"\n", del).replace("\n" + rev, rev).replace(/\n/g, "<br/>"); 
-  
-  console.log( text );
-  regexExpression = "("+del+"|"+rev+")";
-  regex = new RegExp(regexExpression, "g");
-  newtext = "";
-  text_hold = "";
-  code_hold = "";
-  state = 0; //1: after code start delimiter but before stop
-  words = text.split( regex );
-  console.log( words );
-  for(i = 0; i < words.length; i++){
-      if( state == 0){
+    console.log( t) ;
+      
+    var text = t.replace(del+"\n", del).replace("\n" + rev, rev).replace(/\n/g, "<br/>"); 
+      
+    console.log( text );
+    regexExpression = "("+del+"|"+rev+")";
+    regex = new RegExp(regexExpression, "g");
+    newtext = "";
+    text_hold = "";
+    code_hold = "";
+    state = 0; //1: after code start delimiter but before stop
+    words = text.split( regex );
+    console.log( words );
+    for(i = 0; i < words.length; i++){
+    if( state == 0){
         if( words[i] == del){
             newtext += "<p class='inline'>" + text_hold + "</p>";
             state = 1;
@@ -116,11 +137,21 @@ function write(t){
     console.log( newtext );
     return newtext;
 }
+
 var checkForMsg = setInterval(function() {
-    var msgWindow = document.getElementById('ChatTabsPagelet');
+    var msgWindow = document.getElementById('webMessengerRecentMessages');
     if (msgWindow !== null) {
         console.log('found!');
         monospaceListen(msgWindow, observerConfig);
         clearInterval(checkForMsg);
+    }
+}, 50);
+
+var SMcheckForMsg = setInterval(function() {
+    var SMmsgWindow = document.getElementById('ChatTabsPagelet');
+    if (SMmsgWindow !== null) {
+        console.log('SMfound!');
+        SMmonospaceListen(SMmsgWindow, observerConfig);
+        clearInterval(SMcheckForMsg);
     }
 }, 50);
