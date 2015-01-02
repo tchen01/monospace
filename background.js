6fcdc6758eed5a9a38247d3648c5fcf62ad22a4a
Tyler Chen <chentylerl@gmail.com>
2015-01-02 17:10:25 -0600

    inject css rather than use content script
    styles relating to themes should be removed from main.css so that skins
    can work properly

--- background.js
+++ background.js
@@ -2,7 +2,11 @@
  * background script for app.js code injection
  * @author Jesse Mu, Tyler Chen
  */
- 
+
+var highlightjs = document.createElement("script");
+highlightjs.src = chrome.extension.getURL("highlight/highlight.pack.js");
+document.head.appendChild(highlightjs);
+
 var script = document.createElement("script");
 script.id = 'monospace';
 script.src = chrome.extension.getURL("app.js");
@@ -10,22 +14,25 @@ document.head.appendChild(script);
 
 var monospace = document.getElementById('monospace');
 
-//inject css page rather than having it as a content script?
-var highlightjs = document.createElement("script");
-highlightjs.src = chrome.extension.getURL("highlight/highlight.pack.js");
-document.head.appendChild(highlightjs);
+var style = document.createElement("link");
+style.rel = "stylesheet";
+style.id = "monospaceStyles"
+style.href = chrome.extension.getURL("highlight/styles/default.css");
+document.head.appendChild(style);
 
-//re-inject script (and appropriate CSS file) on update or have listener in app.js?
+var monospaceStyles = document.getElementById('monospaceStyles');
+
+//change monospaceStyles.href on update.
+//reinject script or have listener in app.js???
 chrome.storage.onChanged.addListener(function(changes, namespace) {
     updateVars()
     for (key in changes) {
       var storageChange = changes[key];
-      console.log('Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".', key, namespace,storageChange.oldValue,storageChange.newValue);}
+      //console.log('Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".', key, namespace,storageChange.oldValue,storageChange.newValue);}
     });
 
 function updateVars(){
     chrome.storage.sync.get(function(e){
-        console.log( e );
         var vars = '';
         for (var key in e) {
             vars += '"' + key + '":"' + e[key] + '",';
