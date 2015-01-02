d82dcbbb91b967e4be13632b92e3091d378021a6
Tyler Chen <chentylerl@gmail.com>
2015-01-02 11:22:15 -0600

    reformat background.js
    commented possibilities for updating variables.

--- background.js
+++ background.js
@@ -10,23 +10,19 @@ document.head.appendChild(script);
 
 var monospace = document.getElementById('monospace');
 
+//inject css page rather than having it as a content script?
 var highlightjs = document.createElement("script");
 highlightjs.src = chrome.extension.getURL("highlight/highlight.pack.js");
 document.head.appendChild(highlightjs);
 
+//re-inject script (and appropriate CSS file) on update or have listener in app.js?
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-        updateVars()
-        for (key in changes) {
-          var storageChange = changes[key];
-          console.log('Storage key "%s" in namespace "%s" changed. ' +
-                      'Old value was "%s", new value is "%s".',
-                      key,
-                      namespace,
-                      storageChange.oldValue,
-                      storageChange.newValue);
-        }
-      });
-      
+    updateVars()
+    for (key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".', key, namespace,storageChange.oldValue,storageChange.newValue);}
+    });
+
 function updateVars(){
     chrome.storage.sync.get(function(e){
         console.log( e );
